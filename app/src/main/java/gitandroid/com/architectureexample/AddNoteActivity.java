e18563f78e53386f7aea05fbe75fa4a821dfeb54
gitandroids <okyayonur1@gmail.com>
2020-07-08 15:48:52 +0300

    AddNoteActivity created.Floated action and BacktoMainActivity added.StartActivityForResult method was used.

---
+++ app/src/main/java/gitandroid/com/architectureexample/AddNoteActivity.java
@@ -0,0 +1,80 @@
+package gitandroid.com.architectureexample;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+public class AddNoteActivity extends AppCompatActivity {
+    //psfs + TAB = public static final String ..
+    public static final String EXTRA_TITLE =
+            "gitandroid.com.architectureexample.EXTRA_TITLE";
+    public static final String EXTRA_DESCRIPTION =
+            "gitandroid.com.architectureexample.EXTRA_DESCRIPTION";
+    public static final String EXTRA_PRIORITY =
+            "gitandroid.com.architectureexample.EXTRA_PRIORITY";
+
+    private EditText editTextTitle;
+    private EditText editTextDescription;
+    private NumberPicker numberPickerPriority;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextDescription = findViewById(R.id.edit_text_description);
+        numberPickerPriority = findViewById(R.id.number_picker_priority);
+
+        numberPickerPriority.setMinValue(1);
+        numberPickerPriority.setMaxValue(10);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        setTitle("Add Note");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return true;  // We want this menu to show .
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_note:
+                saveNote();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+    // Normally we could call another ViewModel here. But for this exercise we do the database operations just from MainActivity.
+    //So that we are going to start AddNoteActivity with startActivityforResult from MainActivity.
+    // That means we can get the values back to main activity from this Activity and addNote in MainActivity using the ViewModel there.
+
+    private void saveNote() {
+        String title = editTextTitle.getText().toString();
+        String description = editTextDescription.getText().toString();
+        int priority = numberPickerPriority.getValue();
+        if (title.trim().isEmpty() || description.trim().isEmpty()) {  // trim() empthy spaces and look if the String is empthy.
+            Toast.makeText(this, "Please insert a title and description", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Intent data = new Intent();
+        data.putExtra(EXTRA_TITLE, title);
+        data.putExtra(EXTRA_DESCRIPTION, description);
+        data.putExtra(EXTRA_PRIORITY, priority);
+        setResult(RESULT_OK,data);  // We indicate if the input was successful or not. We will get these values in MainActivity.
+        finish();
+    }
+}
